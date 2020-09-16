df3d8fae028966a66e4eaa1ef8c84820df210b97
hmccain <hmccain@vetsfirstchoice.com>
2020-09-16 10:04:31 -0700

    Add challenge criteria.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Welcome!
       </header>
+      <p>
+        Hi there! For your challenge, please implement (or begin implementing) a proof-of-concept for an app that fulfills the following 3.5 criteria:
+      </p>
+      <ol>
+        <li>Implements a form where the user can enter their name and choose their favorite color (input, dropdown, selector — however you want to do it)</li>
+          {/* eslint-disable-next-line react/jsx-no-target-blank */}
+        <li>On submit, fetch a list of "nouns that are often described by the adjective `userColor`," using this API: <a href={"http://www.datamuse.com/api/"} target={"_blank"}>http://www.datamuse.com/api/</a></li>
+        <li>Display the results, AND allow the user the option to return to the form or otherwise enter a new color</li>
+      </ol>
+      <p>Use whatever resources you need to complete this challenge. Good luck!</p>
     </div>
   );
 }
